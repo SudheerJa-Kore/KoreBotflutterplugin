0e621248900fd7a4d0290e39f465da2fef52ae99
Kartheek Pagidimarri <Kartheek.pagidimarri@kore.com>
2023-08-11 12:20:32 +0530

    iOS bot config changes

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -41,11 +41,11 @@ import korebotplugin
     
     func setBotConfig(){
         
-        let clientId = "cs-35a729ed-028d-5428-96b4-118db6f36d52"; // Copy this value from Bot Builder SDK Settings ex. cs-5250bdc9-6bfe-5ece-92c9-ab54aa2d4285
+        let clientId = "cs-1e845b00-81ad-5757-a1e7-d0f6fea227e9"; // Copy this value from Bot Builder SDK Settings ex. cs-5250bdc9-6bfe-5ece-92c9-ab54aa2d4285
         
-        let clientSecret = "pqtjXJSDBYfKehsNdbrBPxQVXvlZIMqm8hxM5gXV724="; // Copy this value from Bot Builder SDK Settings ex. Wibn3ULagYyq0J10LCndswYycHGLuIWbwHvTRSfLwhs=
+        let clientSecret = "5OcBSQtH/k6Q/S6A3bseYfOee02YjjLLTNoT1qZDBso="; // Copy this value from Bot Builder SDK Settings ex. Wibn3ULagYyq0J10LCndswYycHGLuIWbwHvTRSfLwhs=
         
-        let botId = "st-b7cde454-5022-5f5a-80ee-e6755849a235"; // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. st-acecd91f-b009-5f3f-9c15-7249186d827d
+        let botId = "st-b9889c46-218c-58f7-838f-73ae9203488c"; // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. st-acecd91f-b009-5f3f-9c15-7249186d827d
         
         let chatBotName = "SDKBot"; // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. "Demo Bot"
         
@@ -55,7 +55,7 @@ import korebotplugin
         
         let JWT_SERVER = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/"; // Replace it with the actual JWT server URL, if required. Refer to developer documentation for instructions on hosting JWT Server.
         
-        let BOT_SERVER = "https://bankassist.kore.ai";
+        let BOT_SERVER = "https://bots.kore.ai";
         
         botConnect.initialize(clientId, clientSecret: clientSecret, botId: botId, chatBotName: chatBotName, identity: identity, isAnonymous: isAnonymous, JWTServerUrl: JWT_SERVER, BOTServerUrl: BOT_SERVER);
     }
