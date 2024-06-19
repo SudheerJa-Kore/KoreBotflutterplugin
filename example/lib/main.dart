c6683c2350564be2c2ed314f0b5f5c2502125f98
SudheerJa-Kore <sudheer.jampana@kore.com>
2024-06-19 12:38:55 +0530

    Flutter plugin version upgrade

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -70,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
         "jwt_server_url":
             "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/",
         "server_url": "https://bots.kore.ai",
-        "callHistory": true
+        "callHistory": false
       });
     } on PlatformException {}
   }
